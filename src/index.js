413883b40b49806f69f3013d04e20dd4e2c1102e
Edinson <edinson.ningles@gmail.com>
2022-04-11 20:57:30 +0200

    ~init

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
 import { component } from "./component";
-import { logo } from "./logo";
+// import { logo } from "./logo";
+import "./index.css";
 import "./app.css";
-import "./app.scss";
-document.body.appendChild(component());
-document.body.appendChild(logo());
+
+async function appendData() {
+  const response = await fetch('https://flapotest.blob.core.windows.net/test/ProductData.json');
+  const json = await response.json();
+  // console.log(json[0].name);
+  // console.log(json[0].articles[0].price)
+  // console.log(json[0].articles[0].image)
+
+  let mainContainer = document.getElementById('data');
+
+  for(let i = 0; i < json.length; i ++) {
+    let div = document.createElement("div");
+    let img = document.createElement("img");
+    div.innerHTML = json[i].name + '</br>' + json[i].articles[0].price +  '</br>' + '<img src="' + json[i].articles[0].image + '">' + '</br></br>';
+  
+    mainContainer.appendChild(div);
+    // var obj = json[i];
+    // console.log(obj.name)
+    // console.log(obj.articles[0].price)
+    // console.log(obj.articles[0].image)
+
+    
+  }
+}
+
+appendData();
+
+
+// document.body.appendChild(component());
+// document.body.appdendChild()
+// document.body.appendChild(logo());
